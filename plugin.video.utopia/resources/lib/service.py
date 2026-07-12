a873804e3fadf4ad62041d03dffc6469b629aceb
dude2714 <dude2714@example.com>
2026-07-11 19:12:05 -0700

    Sync Utopia addon/repo from local Kodi

---
+++ plugin.video.utopia/resources/lib/service.py
@@ -0,0 +1,14 @@
+﻿from entry import logger, UtopiaMonitor
+
+if __name__ == '__main__':
+	logger('Utopia', 'Main Monitor Service Starting')
+	logger('Utopia', 'Settings Monitor Service Starting')
+
+	with UtopiaMonitor() as Utopia:
+		Utopia.startUpServices()
+		Utopia.waitForAbort()
+
+	logger('Utopia', 'Settings Monitor Service Finished')
+	logger('Utopia', 'Main Monitor Service Finished')
+
+
